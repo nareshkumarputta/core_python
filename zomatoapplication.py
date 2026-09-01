5c043d47594fb4e8e34732ee651336162bb1c408
nareshkumarputta <nareshkumarputtaa@gmail.com>
2026-09-01 20:44:31 +0530

    up

--- zomatoapplication.py
+++ zomatoapplication.py
@@ -5,9 +5,19 @@ class zomato:
     restuarent_number=0
     def __init__(self,restuarent_name):
         self.restuarent_name=restuarent_name
-        self.item={1:{'name':'chicken_biryani','price':300},
-                    2:{'name':'mutton_biryani','price':650},
-                    3:{'name':'coke','price':50}}
+        self.item={
+                    1: {"name": "Pizza", "price": 250},
+                    2: {"name": "Burger", "price": 150},
+                    3: {"name": "Pasta", "price": 200},
+                    4: {"name": "Sandwich", "price": 120},
+                    5: {"name": "French Fries", "price": 100},
+                    6: {"name": "Noodles", "price": 180},
+                    7: {"name": "Egg_Biryani", "price": 250},
+                    8: {"name": "Chicken Wings", "price": 300},
+                    9: {"name": "Ice Cream", "price": 80},
+                    10:{"name": "Cold Drink", "price": 50},
+                    11:{"name":"chicken_biryani","price":560},
+                    12:{"name":"mutton_biryani","price":780}}
         zomato.restuarent_number+=1
         self.restuarent_id=zomato.restuarent_number
         if restuarent_name in zomato.lor:
@@ -15,9 +25,10 @@ class zomato:
             for key,value in self.item.items():
                     print(f"{key}:{value}")
         else:
-            print("Enter a valid restuarent")
+            print("Restuarent not available")
     def order(self,n):
         if n in self.item:
+            print("Your Selected Item is:",self.item[n]['name'])
             self.price=self.item[n]['price']
             p=input("Enter Your Coupon Code:")
             if p==zomato.coupon_code:
